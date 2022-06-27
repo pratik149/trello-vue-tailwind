0b2f36f1cd5891adaf217862f6a8940c62777f3f
Pratik Rane <pratikrane149@gmail.com>
2022-06-27 08:33:14 +0530

    fix: :ambulance: import main.css in main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 import App from "./App.vue";
 import router from "./router";
+import "./assets/main.css";
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
