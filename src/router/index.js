6753e95fd299ac6515c63c27fd48fa4ae88ff2a7
Pratik Rane <pratikrane149@gmail.com>
2022-06-27 17:35:16 +0530

    build: :wrench: add .env file

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
+console.log(import.meta.env.BASE_URL);
 const router = new createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
