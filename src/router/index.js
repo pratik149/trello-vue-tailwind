e35d6f698656b71c1bdd3a48f715df41aec4bae5
Pratik Rane <pratikrane149@gmail.com>
2022-06-27 08:32:12 +0530

    refactor: :sparkles: update router in support of vue3

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,23 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
-Vue.use(VueRouter)
-
-const router = new VueRouter({
-  mode: 'history',
-  base: import.meta.env.BASE_URL,
+const router = new createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-		path: '/board',
-		name: 'board',
-		// route level code-splitting
-		// this generates a separate chunk (Board.[hash].js) for this route
-		// which is lazy-loaded when the route is visited.
-		component: () => import('../views/BoardView.vue')
-	  }
-  ]
-})
+      path: "/board",
+      name: "board",
+      // route level code-splitting
+      // this generates a separate chunk (Board.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/BoardView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
