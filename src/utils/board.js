ab6436184c3775c5a934d5985fe0660befef8e7f
Pratik Rane <pratikrane149@gmail.com>
2022-06-27 08:37:46 +0530

    feat: :sparkles: create commonly used util functions for board

---
+++ src/utils/board.js
@@ -0,0 +1,28 @@
+import { v1 as uuidv1 } from "uuid";
+
+export function makeItem(title, description, date, id = null) {
+  id = id || uuidv1();
+  return { id, title, description, date };
+}
+
+export function makeList(title, items = []) {
+  const id = uuidv1();
+  return { id, title, items };
+}
+
+export function getListById(lists, listId) {
+  return lists.find((list) => list.id === listId);
+}
+
+export function getListByItemId(lists, itemId) {
+  return lists.find((list) => list.items.find((item) => item.id === itemId));
+}
+
+export function getItemById(lists, itemId) {
+  const list = lists.find((list) =>
+    list.items.find((item) => item.id === itemId)
+  );
+  if (list) {
+    return list.items.find((item) => item.id === itemId);
+  }
+}
