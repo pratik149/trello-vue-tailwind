d575f7eb4c6e65bfb12c7bdb7ec47389b004522c
Pratik Rane <pratikrane149@gmail.com>
2022-06-28 02:05:43 +0530

    fix: :lipstick: add backdrop shadow when open create list form modal

--- src/components/AddListForm.vue
+++ src/components/AddListForm.vue
@@ -20,7 +20,7 @@
 						leave-from="opacity-100"
 						leave-to="opacity-0"
 					>
-						<DialogOverlay class="fixed inset-0" />
+						<DialogOverlay class="fixed inset-0 bg-black bg-opacity-50" />
 					</TransitionChild>
 
 					<span class="inline-block h-screen align-middle" aria-hidden="true"> &#8203; </span>
