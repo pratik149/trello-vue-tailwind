57d0acd8d277a25c5ba978b645d82215bad77c26
Pratik Rane <pratikrane149@gmail.com>
2022-06-27 15:45:58 +0530

    feat: :sparkles: update `BoardCard` and `BoardList` components

--- src/components/BoardCard.vue
+++ src/components/BoardCard.vue
@@ -1,14 +1,44 @@
-<script setup>
-import { RouterLink } from "vue-router";
-</script>
-
 <template>
 	<li class="group relative bg-white p-3 shadow rounded-md border-b border-gray-300 hover:bg-gray-50">
-		<RouterLink to="#" class="text-sm">card item</RouterLink>
-		<button
-			class="hidden absolute top-1 right-1 w-8 h-8 bg-gray-50 group-hover:grid place-content-center rounded-md text-gray-600 hover:text-black hover:bg-gray-200"
-		>
-			✏️
-		</button>
+		<div>
+			<div>
+				<h5 class="font-semibold">{{ task.title }}</h5>
+				<p>{{ task.date }}</p>
+			</div>
+			<div class="hidden absolute top-1 right-1 group-hover:grid">
+				<EditCardForm :task="task" />
+				<button
+					@click="deleteTask"
+					class="w-8 h-8 bg-gray-50 place-content-center rounded-md text-gray-600 hover:text-black hover:bg-gray-200"
+				>
+					❌
+				</button>
+			</div>
+		</div>
 	</li>
 </template>
+
+<script setup>
+import { useBoardStore } from "@/stores/board";
+import EditCardForm from "./EditCardForm.vue";
+const props = defineProps({ task: Object, listId: String });
+
+const boardStore = useBoardStore();
+
+function deleteTask() {
+	boardStore.removeItem({ itemId: props.task.id });
+}
+</script>
+
+<style scoped>
+.drag > div {
+	transform: rotate(5deg);
+}
+.ghost {
+	background: lightgray;
+	border-radius: 6px;
+}
+.ghost > div {
+	visibility: hidden;
+}
+</style>
--- src/components/BoardList.vue
+++ src/components/BoardList.vue
@@ -1,29 +1,55 @@
-<script setup>
-import BoardCard from "./BoardCard.vue";
-</script>
-
 <template>
 	<div class="w-72 bg-gray-200 max-h-full flex flex-col rounded-md">
 		<div class="flex items-center justify-between px-3 py-2">
-			<h3 class="text-sm font-semibold text-gray-700">Backlog</h3>
-			<button class="hover:bg-gray-300 w-8 h-8 rounded-md grid place-content-center">⚙️</button>
+			<h3 class="text-sm font-semibold text-gray-700">{{ list.title }}</h3>
+			<button class="hover:bg-gray-300 w-8 h-8 rounded-md grid place-content-center" @click="removeList">
+				❌
+			</button>
 		</div>
 
 		<div class="pb-3 flex flex-col overflow-hidden">
 			<div class="px-3 flex-1 overflow-y-auto">
-				<ul class="space-y-3">
-					<BoardCard v-for="(item, i) in Array.from({ length: 7 })" :key="i" />
-				</ul>
+				<Draggable
+					v-model="cards"
+					group="cards"
+					item-key="id"
+					class="space-y-3"
+					tag="ul"
+					drag-class="drag"
+					ghost-class="ghost"
+					@change="onChange"
+				>
+					<template #item="{ element }">
+						<BoardCard :task="element" :list-id="list.id" />
+					</template>
+				</Draggable>
 			</div>
 
 			<div class="px-3 mt-3">
-				<button
-					class="flex items-center p-2 text-sm font-medium text-gray-600 hover:text-black hover:bg-gray-300 w-full rounded-md"
-				>
-					➕
-					<span class="ml-1">Add card</span>
-				</button>
+				<AddCardForm :list-id="list.id" />
 			</div>
 		</div>
 	</div>
 </template>
+
+<script setup>
+import BoardCard from "./BoardCard.vue";
+import AddCardForm from "./AddCardForm.vue";
+import { useBoardStore } from "@/stores/board";
+import Draggable from "vuedraggable";
+import { ref } from "vue";
+
+const boardStore = useBoardStore();
+const props = defineProps({ list: Object });
+const cards = ref(props.list.items);
+
+function removeList() {
+	boardStore.removeList({
+		listId: props.list.id,
+	});
+}
+
+function onChange(e) {
+	console.log(e);
+}
+</script>
