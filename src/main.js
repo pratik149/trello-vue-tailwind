0e3d4014fc6dabfa303ce83f03a5bdfada7560c0
Pratik Rane <pratikrane149@gmail.com>
2022-06-27 16:28:18 +0530

    chore: :wrench: configure `dayjs` lib

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import dayjs from "dayjs";
 
 import App from "./App.vue";
 import router from "./router";
 import "./assets/main.css";
 
+const app = createApp(App);
 const pinia = createPinia();
 
-const app = createApp(App);
-
+app.config.globalProperties.$dayjs = dayjs;
 app.use(pinia);
 app.use(router);
 
