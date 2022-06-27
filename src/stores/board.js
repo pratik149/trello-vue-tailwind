05e8bd16c75c7934fc1a117f5ace30f9904d77fd
Pratik Rane <pratikrane149@gmail.com>
2022-06-27 14:52:19 +0530

    feat: :sparkles: update store actions

--- src/stores/board.js
+++ src/stores/board.js
@@ -4,9 +4,8 @@ import { makeItem, makeList } from "../utils/board";
 
 export const useBoardStore = defineStore({
 	id: "board",
-	persist: true,
 	state: () => ({
-		lists: [1, 2, 3],
+		lists: [],
 	}),
 	getters: {
 		getListById: (state) => (listId) => {
@@ -20,43 +19,25 @@ export const useBoardStore = defineStore({
 		},
 	},
 	actions: {
-		lists(state, value) {
-			state.lists = value;
+		addList({ title }) {
+			this.lists.push(makeList(title));
 		},
-		reset(state) {
-			state.lists = [];
+		removeList({ listId }) {
+			const index = this.lists.findIndex((list) => list.id === listId);
+			this.lists.splice(index, 1);
 		},
-		addList(state, { title }) {
-			state.lists.push(makeList(title));
+		addItem({ listId, title, description, date }) {
+			const list = getListById(this.lists, listId);
+			list.items.push(makeItem(title, description, date, null));
 		},
-		moveList(state, [fromIndex, toIndex]) {
-			state.lists.splice(toIndex, 0, state.lists.splice(fromIndex, 1)[0]);
-		},
-		removeList(state, { listId }) {
-			const index = state.lists.findIndex((list) => list.id === listId);
-			state.lists.splice(index, 1);
-		},
-		addItem(state, { listId, title, description, date }) {
-			const list = getListById(state.lists, listId);
-			list.items.push(makeItem(title, description, date));
-		},
-		updateItem(state, { itemId, title, description, date }) {
-			const item = getItemById(state.lists, itemId);
+		updateItem({ itemId, title, description, date }) {
+			const item = getItemById(this.lists, itemId);
 			if (item) {
 				Object.assign(item, makeItem(title, description, date, itemId));
 			}
 		},
-		moveItem(state, [fromListRef, fromIndex, toListRef, toIndex]) {
-			const fromList =
-				typeof fromListRef === "number"
-					? state.lists[fromListRef].items
-					: getListById(state.lists, fromListRef);
-			const toList =
-				typeof toListRef === "number" ? state.lists[toListRef].items : getListById(state.lists, toListRef);
-			toList.splice(toIndex, 0, fromList.splice(fromIndex, 1)[0]);
-		},
-		removeItem(state, { itemId }) {
-			const list = getListByItemId(state.lists, itemId);
+		removeItem({ itemId }) {
+			const list = getListByItemId(this.lists, itemId);
 			list.items.splice(
 				list.items.findIndex((item) => item.id === itemId),
 				1
@@ -64,5 +45,3 @@ export const useBoardStore = defineStore({
 		},
 	},
 });
-
-export default useBoardStore;
