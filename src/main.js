fea349f4a72f50da6b46a9e29dba737101062584
Pratik Rane <pratikrane149@gmail.com>
2022-06-27 14:50:06 +0530

    build: :heavy_minus_sign: npm uninstall `pinia-plugin-persistedstate`

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 import App from "./App.vue";
 import router from "./router";
 import "./assets/main.css";
 
 const pinia = createPinia();
-pinia.use(piniaPluginPersistedstate);
 
 const app = createApp(App);
 
