ce8d5dc609760a7e1ebbd8936efbcb4eeef59651
Pratik Rane <pratikrane149@gmail.com>
2022-06-28 16:25:01 +0530

    feat: :lipstick: update home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,21 +3,31 @@ import { RouterLink } from "vue-router";
 </script>
 
 <template>
-	<main class="py-20">
-		<div class="max-w-md text-center mx-auto">
+	<main class="flex flex-col h-screen justify-around items-center">
+		<div class="text-center max-w-md">
 			<h1 class="text-2xl font-semibold text-gray-700">Welcome to Trello Vue</h1>
 			<p class="text-gray-500 mt-2">
 				A kanban board is an agile project management tool designed to help visualize work, limit
 				work-in-progress, and maximize efficiency (or flow).
 			</p>
 		</div>
-		<div class="flex justify-center mt-4">
+
+		<div>
 			<RouterLink
 				to="/board"
 				class="text-center text-3xl font-extrabold text-gray-800 rounded-lg bg-blue-100 px-4 py-2 hover:bg-blue-600 hover:text-white transition ease-out duration-150"
 			>
-				Trello Board
+				Explore Board
 			</RouterLink>
+		</div>
+
+		<div>
+			<p class="text-center text-gray-600">
+				Made with ❤️ by
+				<a href="https://github.com/pratik149/" class="text-blue-500 hover:text-blue-600" target="_blank"
+					>@pratik149</a
+				>
+			</p>
 		</div>
 	</main>
 </template>
